0856cb961661691a9ea31dba0a346c541643ab36
Brynjar <brynjar.blomvik@gmail.com>
2021-07-24 07:28:04 +0200

    init botnet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Net from './botnet/Net';
+import FrontEnd from './FrontEnd';
+import Bot from './botnet/Bot';
+import CommandAndControl from './botnet/CommandAndControl';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Net />
       </header>
     </div>
   );
