e303712ebb8208143061e3805c50d4531477922c
Brynjar <brynjar.blomvik@gmail.com>
2021-09-15 23:22:12 +0200

    crude login

--- src/Authorizer.jsx
+++ src/Authorizer.jsx
@@ -3,21 +3,20 @@ import Login from './Login';
 import GameRouter from './GameRouter';
 
 export default function Authorizer() {
-	const [loggedIn, setLoggedIn] = useState(false);
-	const [loggedInUser, setLoggedInUser] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState(null);
 
-	return (
-		<div>
-			{loggedIn ? (
-				<div>
-					{/* 	// <GameRouter logout={() => setLoggedIn(false)} /> */}
-					{loggedIn && 'name placeholder'}
-					<br />
-					<button onClick={() => setLoggedIn(false)}>log out</button>
-				</div>
-			) : (
-				<Login login={() => setLoggedIn(true)} />
-			)}
-		</div>
-	);
+  return (
+    <div>
+      {loggedIn ? (
+        <div>
+          <button onClick={() => setLoggedIn(false)}>log out</button>
+          {loggedIn && 'name placeholder'}
+          <GameRouter logout={() => setLoggedIn(false)} />
+        </div>
+      ) : (
+        <Login login={() => setLoggedIn(true)} />
+      )}
+    </div>
+  );
 }
