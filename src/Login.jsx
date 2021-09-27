30726fd7bcf730e5c41f4efe9a8d0499cb331758
Brynjar Blomvik XPS <brynjar.blomvik@gmail.com>
2021-09-27 16:57:22 +0200

    fix typo

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
 
 export default function Login(props) {
-  const [player1Name, setPlayer1Name] = useState('');
-  const [player2Name, setPlayer2Name] = useState('');
-  return (
-    <div>
-      <p>what yor name boi</p>
-      <input
-        type='text'
-        value={player1Name}
-        onChange={(event) => setPlayer1Name(event.target.value)}
-      />
-      <br />
-      <p>who de enemi</p>
-      <input
-        type='text'
-        value={player2Name}
-        onChange={(event) => setPlayer2Name(event.target.value)}
-      />
+	const [player1Name, setPlayer1Name] = useState('');
+	const [player2Name, setPlayer2Name] = useState('');
+	return (
+		<div>
+			<p>what yor name boy</p>
+			<input
+				type='text'
+				value={player1Name}
+				onChange={(event) => setPlayer1Name(event.target.value)}
+			/>
+			<br />
+			<p>Your opponent</p>
+			<input
+				type='text'
+				value={player2Name}
+				onChange={(event) => setPlayer2Name(event.target.value)}
+			/>
 
-      <button onClick={() => props.login(player1Name, player2Name)}>
-        log in
-      </button>
-    </div>
-  );
+			<button onClick={() => props.login(player1Name, player2Name)}>
+				log in
+			</button>
+		</div>
+	);
 }
